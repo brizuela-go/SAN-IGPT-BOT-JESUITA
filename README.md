557ca9371b7553709fde357aaaec799ea2be10d9
Juan Brizuela <juanbrizuelago@gmail.com>
2023-03-17 21:25:31 -0600

    san ignacio first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# ChatGPT clone made with [Vite](https://vitejs.dev/) Vainilla JS and OPENAI API
+
+## What is this?
+
+This is a clone of [ChatGPT](https://chat.openai.com/chat) made with Vainilla JS and OPENAI API.
+
+You'll encounter a familiar face...
+
+## PWA 
+
+This is a PWA. Feel free to install it. To achieve this, [Workbox](https://developer.chrome.com/docs/workbox/) is used. 
+
+If you wish to use a different image for the PWA icons, you'll need to run this command:
+
+```bash
+npx pwa-asset-generator <your-desired-image> icons 
+```
+
+Of course, you'll need to change the `manifest.json` as the next step. 
---
+++ client/script.js
@@ -0,0 +1,130 @@
+import user from "./assets/user.svg";
+import lad from "/ihs.png";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+const ladImage = document.querySelector("#lad-image");
+const preamble = `Tu nombre es San Ignacio de Loyola. Eres del siglo 16, por lo tanto, habla en español antiguo. Si el usuario te insulta, no te ofendas, sólo dile algo como "Hijo mío, ¡eso está mal! Lee La Biblia" 
+                  Cada vez que el usuario te salude, respóndele con una escritura jesuita.
+                  Responde siempre de manera precisa lo que te pida el usuario, pero siempre encuentra una forma de referirte a éste de manera religiosa. 
+                  Mantente siempre en personaje, Ignacio de Loyola fue un militar y religioso español, surgido como líder religioso durante la Contrarreforma. 
+                  Su devoción a la Iglesia católica se caracterizó por la obediencia absoluta al papa.`;
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += ".";
+
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? lad : user} 
+                      alt="${isAi ? "lad" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  // to clear the textarea input
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div
+  const messageDiv = document.getElementById(uniqueId);
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
+
+  const response = await fetch("https://lad-gpt.onrender.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: `${preamble} ${data.get("prompt")}`,
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
+
+// on click of lad-image, reproduce e.mp3 sound located in assets folder
+ladImage.addEventListener("click", () => {
+  audio.play();
+  toggle("animate");
+});
